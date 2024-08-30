07da5a176442532fc101a066060261ca59adb423
Suyoung Moon <99530280+MoonS1120@users.noreply.github.com>
2024-08-30 11:28:26 +0800

    Update Denary-Binary.py

--- Denary-Binary.py
+++ Denary-Binary.py
@@ -1,28 +1,25 @@
-import time
-
 line = [' ']*8 
 
 def print_box(line):
     for s in line:
         print('|' + s, end='')
-    print('|')
+    print('|', end='')
 
 def denary_to_binary(n, depth=0):
+    input()
     if n == 0:
         return "0"
     
     if n > 1:
-        time.sleep(1.1)
-        print(f"{n}/2 = {n // 2} remainder {n % 2}".rjust(23))
+        print(f"{n}/2 = {n // 2} remainder {n % 2}".rjust(23), end='')
         return denary_to_binary(n // 2, depth + 1) + str(n % 2)
     else:
-        time.sleep(1.1)
-        print(f"{n}/2 = {n // 2} remainder {n % 2}".rjust(23))
+        print(f"{n}/2 = {n // 2} remainder {n % 2}".rjust(23), end='')
         return str(n % 2)
 
 def denary_to_binary_2(n, depth=7):
     if n > 0:
-        time.sleep(0.7)
+        input()
         line[depth] = str(n % 2)
         print(f"{n}/2  ".rjust(7), end='')
         print_box(line)
@@ -39,27 +36,23 @@ def main():
         except ValueError:
             print("Input a valid number.")
 
-
-    print("Step 1: Continuously divide the number by 2, noting down the quotient and remainder each time, until the quotient reaches 0.\n")
-    time.sleep(3)
+    print("(Press enter to show next step)\n")
+    print("Step 1: Continuously divide the number by 2, noting down the quotient and remainder each time, until the quotient reaches 0.")
     denary_to_binary(num)
-    time.sleep(1)
-    print("\nStep 2: Write the numbers in order from right to left.\n")
-    time.sleep(3)
+    print("\n\nStep 2: Write the numbers in order from right to left.")
     denary_to_binary_2(num)
-    time.sleep(1)
     print('')
 
     if ' ' in line:
         print("\nStep 3: Add the 0s and check you have 8 numbers\n")
-        time.sleep(3)
         for i in range(8):
             if line[i] == ' ':
                 line[i] = '0'
         print(' '*6, end='')
+        input()
         print_box(line)
+        print('')
         
-    time.sleep(1)
     print("\nFinal answer: ", *line)
 
 main()
